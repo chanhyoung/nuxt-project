85ed867866710196e26f7f8562f1d7cd850f03ee
chanhyoung <chlee3927@gmail.com>
2025-04-27 22:57:12 +0900

    nuxtApp 적용연습습

--- app.vue
+++ app.vue
@@ -3,4 +3,10 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// const nuxtApp = useNuxtApp()
+// nuxtApp.provide('hello', (name: string) => `Hello ${name}!`)
+
+// "Hello name!"을 출력합니다.
+// console.log(nuxtApp.$hello('name'))
+</script>

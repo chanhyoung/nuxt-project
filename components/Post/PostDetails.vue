5850710b8703f83c2644ff88deef006c68d25eb5
chanhyoung <chlee3927@gmail.com>
2025-07-14 21:47:16 +0900

    tags 와 category 추가

--- components/Post/PostDetails.vue
+++ components/Post/PostDetails.vue
@@ -1,18 +1,40 @@
 <template>
   <q-card class="q-pa-lg" flat bordered>
     <div class="flex q-mb-md">
-      <q-btn icon="arrow_back" flat round dense color="grey" size="16px"></q-btn>
+      <q-btn
+        icon="arrow_back"
+        flat
+        round
+        dense
+        color="grey"
+        size="16px"
+        @click="$router.back()"
+      ></q-btn>
       <q-space></q-space>
-      <q-btn icon="favorite_border" flat round dense color="red" size="16px"></q-btn>
-      <q-btn icon="bookmark_border" flat round dense color="blue" size="16px"></q-btn>
+      <q-btn
+        icon="favorite_border"
+        flat
+        round
+        dense
+        color="red"
+        size="16px"
+      ></q-btn>
+      <q-btn
+        icon="bookmark_border"
+        flat
+        round
+        dense
+        color="blue"
+        size="16px"
+      ></q-btn>
     </div>
     <div class="flex items-center">
       <q-avatar size="sm">
-        <img src="/images/boy-avatar.png" alt="">
+        <img src="/images/boy-avatar.png" alt="" />
       </q-avatar>
       <div class="q-ml-md">
         <div>짐코딩</div>
-        <div class="text-grey-6">3일 전</div>
+        <div class="text-grey-6">{{ post.createdAt }}</div>
       </div>
       <q-space></q-space>
       <q-btn icon="more_horiz" round flat>
@@ -28,25 +50,51 @@
         </q-menu>
       </q-btn>
     </div>
-    <div class="q-mt-md text-h5 text-weight-bold">제목입니다 - {{ postSlug }}</div>
+    <div class="q-mt-md text-h5 text-weight-bold">
+      {{ post.title }}
+    </div>
+    <div class="flex items-center text-teal">
+      <q-chip class="q-mr-sm" dense color="primary" text-color="white">
+        {{ post.category }}
+      </q-chip>
+      <span v-for="tag in post.tags" :key="tag">#{{ tag }}&nbsp;</span>
+    </div>
     <div class="row items-center q-gutter-x-md q-mt-md justify-end">
-      <PostIcon name="o_visibility" :label="1" tooltip="조회수"></PostIcon>
-      <PostIcon name="o_sms" :label="2" tooltip="댓글수"></PostIcon>
-      <PostIcon name="favorite_border" :label="3" tooltip="좋아요"></PostIcon>
-      <PostIcon name="bookmark_border" :label="4" tooltip="북마크"></PostIcon>
+      <PostIcon
+        name="o_visibility"
+        :label="post.readCount"
+        tooltip="조회수"
+      ></PostIcon>
+      <PostIcon
+        name="o_sms"
+        :label="post.commentCount"
+        tooltip="댓글수"
+      ></PostIcon>
+      <PostIcon
+        name="favorite_border"
+        :label="post.likeCount"
+        tooltip="좋아요"
+      ></PostIcon>
+      <PostIcon
+        name="bookmark_border"
+        :label="post.bookmarkCount"
+        tooltip="북마크"
+      ></PostIcon>
     </div>
     <q-separator class="q-my-lg"></q-separator>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sunt perspiciatis doloremque comodi error, magnam cupiditate ipsum optio consequuntur neque, beatae, maiores delectus molestias cumque debitis sit? aliquid, iste modi.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sunt perspiciatis doloremque comodi error, magnam cupiditate ipsum optio consequuntur neque, beatae, maiores delectus molestias cumque debitis sit? aliquid, iste modi.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sunt perspiciatis doloremque comodi error, magnam cupiditate ipsum optio consequuntur neque, beatae, maiores delectus molestias cumque debitis sit? aliquid, iste modi.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sunt perspiciatis doloremque comodi error, magnam cupiditate ipsum optio consequuntur neque, beatae, maiores delectus molestias cumque debitis sit? aliquid, iste modi.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sunt perspiciatis doloremque comodi error, magnam cupiditate ipsum optio consequuntur neque, beatae, maiores delectus molestias cumque debitis sit? aliquid, iste modi.
-    </div>
+    <pre>{{ post.content }}</pre>
   </q-card>
 </template>
 
 <script setup>
-const route = useRoute();
-const postSlug = route.params.postSlug;
+const route = useRoute()
+const postSlug = route.params.postSlug
+const post = ref(null)
+
+const { getPost } = usePostStore()
+
+const result = await getPost(postSlug)
+post.value = result
+console.log('post: ', post.value)
+// post.value = result
 </script>

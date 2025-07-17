2b3ac82203301fa718104d12036dfa4538984d95
chanhyoung <chlee3927@gmail.com>
2025-07-17 22:20:38 +0900

    변수명 수정(params -> searchParams)

--- pages/community/index.vue
+++ pages/community/index.vue
@@ -2,11 +2,11 @@
   <q-page padding>
     <div class="row q-col-gutter-x-lg">
       <PostLeftBar
-        v-model:category="params.category"
+        v-model:category="searchParams.category"
         class="col-grow"
       ></PostLeftBar>
       <section class="col-7">
-        <PostHeader v-model:sort="params.sort"></PostHeader>
+        <PostHeader v-model:sort="searchParams.sort"></PostHeader>
         <PostList :items="posts"></PostList>
         <div class="flex flex-center q-my-md q-ml-md">
           <q-btn
@@ -19,7 +19,7 @@
         </div>
       </section>
       <PostRightBar
-        v-model:tags="params.tags"
+        v-model:tags="searchParams.tags"
         class="col-3"
         @open-write-dialog="openWriteDialog"
       ></PostRightBar>
@@ -37,7 +37,7 @@ const postDialog = ref(false)
 const { getPosts } = usePostStore()
 const isLastPage = ref(false)
 
-const params = ref({
+const searchParams = ref({
   category: null,
   tags: [],
   sort: 'createdAt',
@@ -45,29 +45,33 @@ const params = ref({
   limit: 2,
 })
 
-const loadPosts = async (p, loadMore = false) => {
-  const result = await getPosts(p)
+const loadPosts = async (searchParams, loadMore = false) => {
+  const result = await getPosts(searchParams)
   if (loadMore) {
     posts.value = [...posts.value, ...result]
   } else {
     posts.value = result
   }
-  if (result.length < params.value.limit) {
+  if (result.length < searchParams.limit) {
     isLastPage.value = true
   }
 }
 
 const loadMore = () => {
-  params.value.page++
-  loadPosts(params.value, true)
+  searchParams.value.page++
+  loadPosts(searchParams.value, true)
 }
 
 watch(
-  () => [params.value.category, params.value.tags, params.value.sort],
+  () => [
+    searchParams.value.category,
+    searchParams.value.tags,
+    searchParams.value.sort,
+  ],
   () => {
-    params.value.page = 1
+    searchParams.value.page = 1
     isLastPage.value = false
-    loadPosts(params.value)
+    loadPosts(searchParams.value)
   },
   {
     deep: true,
@@ -81,9 +85,9 @@ const openWriteDialog = () => {
 
 const writeSuccess = () => {
   postDialog.value = false
-  params.value.page = 1
+  searchParams.value.page = 1
   isLastPage.value = false
-  loadPosts(params.value)
+  loadPosts(searchParams.value)
 }
 </script>
 

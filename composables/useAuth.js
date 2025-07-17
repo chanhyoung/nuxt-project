c376e1f56c564d9c1476cf88821424b12c1b2741
chanhyoung <chlee3927@gmail.com>
2025-07-17 23:54:30 +0900

    fetchWithAuth 인증확인 수정

--- composables/useAuth.js
+++ composables/useAuth.js
@@ -1,65 +1,70 @@
 export const useAuth = () => {
-  const authStore = useAuthStore();
-  const { user, isAuthenticated } = storeToRefs(authStore);
-  
+  const authStore = useAuthStore()
+  const { user, isAuthenticated } = storeToRefs(authStore)
+
   const useFetchWithAuth = async (url, options = {}) => {
-    console.log('useFetchWithAuth: start.');
+    console.log('useFetchWithAuth: start.')
     if (!isAuthenticated.value) {
-      await navigateTo('/auth/login');
-      return Promise.resolve({ data: null, error: null})
-    } 
+      await navigateTo('/auth/login')
+      return Promise.resolve({ data: null, error: null })
+    }
     return useFetch(url, {
       ...options,
       headers: {
         ...options.headers,
-        'Authorization': user.value.token ? `Bearer ${user.value.token}` : undefined
+        Authorization: user.value.token
+          ? `Bearer ${user.value.token}`
+          : undefined,
       },
-      onResponseError( { response }) {
+      onResponseError({ response }) {
         console.log('>>>onResponseError: response: ', response.status)
         if (response.status === 401 || response.status === 403) {
-          const authStore = useAuthStore();
-          const { signOut } = authStore;
-          signOut();
+          const authStore = useAuthStore()
+          const { signOut } = authStore
+          signOut()
         }
-      }
+      },
     })
   }
 
   const fetchWithAuth = async (url, options = {}) => {
-    console.log('fetchWithAuth: start.');
-    
+    console.log('fetchWithAuth: start.')
+
     if (!isAuthenticated.value) {
-      await navigateTo('/auth/login');
-      return Promise.resolve({ data: null, error: null });
+      await navigateTo('/auth/login')
+      // return Promise.resolve({ data: null, error: null })
+      return
     }
 
     const fetData = await $fetch(url, {
       ...options,
       headers: {
         ...options.headers,
-        'Authorization': user.value.token ? `Bearer ${user.value.token}` : undefined
+        Authorization: user.value.token
+          ? `Bearer ${user.value.token}`
+          : undefined,
       },
       onResponseError({ response }) {
-        console.log('>>>onResponseError: response: ', response.status);
+        console.log('>>>onResponseError: response: ', response.status)
         if (response.status === 401 || response.status === 403) {
-          const authStore = useAuthStore();
-          const { signOut } = authStore;
-          signOut();
+          const authStore = useAuthStore()
+          const { signOut } = authStore
+          signOut()
         }
-      }
+      },
     }).catch(error => {
       throw createError({
         statusCode: error.data.code,
         message: error.data.message,
-        fatal: true
-      });
-    });
+        fatal: true,
+      })
+    })
 
-    return fetData;
-  };
+    return fetData
+  }
 
   return {
     fetchWithAuth,
-    useFetchWithAuth
+    useFetchWithAuth,
   }
 }

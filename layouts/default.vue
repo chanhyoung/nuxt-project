b701762d52a632a377d78323a31ceea0d21c02c9
chanhyoung <chlee3927@gmail.com>
2025-07-19 18:30:24 +0900

    Hydration mismatch 해결

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,94 +4,102 @@
       <q-toolbar>
         <q-toolbar-title>Vue Master Course</q-toolbar-title>
         <q-separator dark vertical />
-        <NuxtLink v-slot="{ navigate }" custom to="/">
-          <q-btn stretch flat :label="$t('home')" no-caps @click="navigate()" />
-        </NuxtLink>
-        <q-separator dark vertical />
-        <NuxtLink v-slot="{ navigate }" custom to="/about">
-          <q-btn
-            stretch
-            flat
-            :label="$t('about')"
-            no-caps
-            @click="navigate()"
-          />
-        </NuxtLink>
-        <q-separator dark vertical />
-        <NuxtLink v-slot="{ navigate }" custom to="/community">
-          <q-btn
-            stretch
-            flat
-            :label="$t('community')"
-            no-caps
-            @click="navigate()"
-          />
-        </NuxtLink>
-        <!-- <q-btn
+        <ClientOnly>
+          <NuxtLink v-slot="{ navigate }" custom to="/">
+            <q-btn
+              stretch
+              flat
+              :label="$t('home')"
+              no-caps
+              @click="navigate()"
+            />
+          </NuxtLink>
+          <q-separator dark vertical />
+          <NuxtLink v-slot="{ navigate }" custom to="/about">
+            <q-btn
+              stretch
+              flat
+              :label="$t('about')"
+              no-caps
+              @click="navigate()"
+            />
+          </NuxtLink>
+          <q-separator dark vertical />
+          <NuxtLink v-slot="{ navigate }" custom to="/community">
+            <q-btn
+              stretch
+              flat
+              :label="$t('community')"
+              no-caps
+              @click="navigate()"
+            />
+          </NuxtLink>
+          <!-- <q-btn
           stretch
           flat
           :label="$t('youtube')"
           no-caps
           @click="moveYoutube"
         /> -->
-        <q-separator dark vertical />
-        <NuxtLink v-slot="{ navigate }" custom to="/admin">
-          <q-btn
-            stretch
-            flat
-            :label="$t('admin')"
-            no-caps
-            @click="navigate()"
-          />
-        </NuxtLink>
-        <q-separator dark vertical />
-        <q-btn-dropdown stretch flat no-caps :label="selectedLanguageName">
-          <q-list padding dense>
-            <q-item
-              v-for="{ code, name } in languages"
-              :key="code"
-              v-close-popup
-              clickable
-              :active="code === $i18n.locale"
-              @click="$i18n.locale = code"
-            >
-              <q-item-section>
-                <q-item-label>{{ name }}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
-        <q-separator dark vertical />
-        <NuxtLink
-          v-if="!isAuthenticated"
-          v-slot="{ navigate }"
-          custom
-          to="/auth/login"
-        >
-          <q-btn
-            stretch
-            flat
-            :label="$t('login')"
-            no-caps
-            @click="navigate()"
-          />
-        </NuxtLink>
-        <template v-else>
-          <q-btn
-            stretch
-            flat
-            :label="$t('logout')"
-            no-caps
-            @click="signOut()"
-          />
-          <NuxtLink v-slot="{ navigate }" custom to="/mypage/profile">
-            <q-btn round flat @click="navigate()">
-              <q-avatar>
-                <img src="/images/boy-avatar.png" alt="">
-              </q-avatar>
-            </q-btn>
+          <q-separator dark vertical />
+          <NuxtLink v-slot="{ navigate }" custom to="/admin">
+            <q-btn
+              stretch
+              flat
+              :label="$t('admin')"
+              no-caps
+              @click="navigate()"
+            />
           </NuxtLink>
-        </template>
+          <q-separator dark vertical />
+          <q-btn-dropdown stretch flat no-caps :label="selectedLanguageName">
+            <q-list padding dense>
+              <q-item
+                v-for="{ code, name } in languages"
+                :key="code"
+                v-close-popup
+                clickable
+                :active="code === $i18n.locale"
+                @click="$i18n.locale = code"
+              >
+                <q-item-section>
+                  <q-item-label>{{ name }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+          <q-separator dark vertical />
+          <NuxtLink
+            v-if="!isAuthenticated"
+            v-slot="{ navigate }"
+            custom
+            to="/auth/login"
+          >
+            <q-btn
+              stretch
+              flat
+              :label="$t('login')"
+              no-caps
+              @click="navigate()"
+            />
+          </NuxtLink>
+          <template v-else>
+            <q-btn
+              stretch
+              flat
+              :label="$t('logout')"
+              no-caps
+              @click="signOut()"
+            />
+            <NuxtLink v-slot="{ navigate }" custom to="/mypage/profile">
+              <q-btn round flat @click="navigate()">
+                <q-avatar>
+                  <img src="/images/boy-avatar.png" alt="" />
+                </q-avatar>
+              </q-btn>
+            </NuxtLink>
+          </template>
+        </ClientOnly>
       </q-toolbar>
     </q-header>
     <q-page-container :style="pageContainerStyle">
@@ -106,9 +114,9 @@
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-const authStore = useAuthStore();
-const { user, isAuthenticated } = storeToRefs(authStore);
-const { signOut } = authStore;
+const authStore = useAuthStore()
+const { user, isAuthenticated } = storeToRefs(authStore)
+const { signOut } = authStore
 
 const pageContainerStyle = computed(() => ({
   maxWidth: '1080px',
@@ -138,5 +146,5 @@ const selectedLanguageName = computed(
   () => languages.value.find(lang => lang.code === locale.value)?.name,
 )
 
-watch(locale, (val) => (useCookie('locale').value = val))
+watch(locale, val => (useCookie('locale').value = val))
 </script>
